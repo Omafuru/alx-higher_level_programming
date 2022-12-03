254824aefad7b2c1f9956345f8e5dfce374adfc4
Onome Mafuru <108694957+Omafuru@users.noreply.github.com>
2022-12-03 14:49:19 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-    if idx not in range(0, len(my_list)):
-        return new_list
-    new_list[idx] = element
-    return new_list
+
+
+def max_integer(my_list=[]):
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
