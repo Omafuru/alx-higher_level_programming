3fc5b258dd671452971135b3b72310d529b1d1fd
Onome Mafuru <108694957+Omafuru@users.noreply.github.com>
2022-12-07 19:36:46 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+def complex_delete(a_dictionary, value):
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+
+    return (a_dictionary)
