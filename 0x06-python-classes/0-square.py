49d8ea694ece63c7731620fa05ef038bd5bc14e9
Onome Mafuru <108694957+Omafuru@users.noreply.github.com>
2022-12-20 17:54:06 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+"""Defines a square """
 
-def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
-            break
-    print()
-    return(total)
+
+class Square:
+    """Does nothing"""
+    pass
